e64ff88a0915925dd90bb92300706a4e57243545
labuser <labuser@LABWKS10>
2017-09-05 19:29:30 +0100

    Adding files to repo

---
+++ Day1.ps1
@@ -0,0 +1,43 @@
+﻿########
+#Part 1#
+########
+
+$bracketOpenChar = "("
+$bracketClosedChar = ")"
+$brackets = (gc  S:\Prod\AdventOfCode\RhysE\input.txt) -split ''
+$Counter = 0
+
+foreach($action in $brackets){
+    if($action -eq $bracketOpenChar){
+        $Counter++
+    } 
+    elseif($action -eq $bracketClosedChar){
+        $Counter--
+    }
+}
+$counter
+
+########
+#Part 2#
+########
+
+$bracketOpenChar = "("
+$bracketClosedChar = ")"
+$brackets = (gc  S:\Prod\AdventOfCode\RhysE\input.txt) -split ''
+$Counter = 0
+$position = 0
+
+foreach($action in $brackets){
+    if($action -eq $bracketOpenChar){
+        $Counter++
+    } 
+    elseif($action -eq $bracketClosedChar){
+        $Counter--
+    }
+    $position++
+    if ($Counter -eq -1) 
+    {
+        break
+    }
+}
+$position
---
+++ Day2.ps1
@@ -0,0 +1,56 @@
+﻿########
+#Part 1#
+########
+
+$elfList = (gc  S:\Prod\AdventOfCode\RhysE\inputDay2.txt)
+$Length = 0
+$Width = 0
+$Height = 0
+$TotalNeeded = 0 
+foreach ($present in $elfList) {
+    
+    $dimensions = [int[]]($present -split 'x')
+
+    $Width = $dimensions[0]
+    $Length = $dimensions[1]
+    $Height = $dimensions[2]
+
+    $faceOne = (2 * $Width * $Length)
+    $faceTwo = (2 * $Width * $Height)
+    $faceThree = (2 * $Height * $Length)
+
+    $TotalNeeded += ($faceOne + $faceTwo + $faceThree)
+
+    $SmallFace = ($faceOne,$faceTwo,$faceThree | sort)[0]/2
+
+    $TotalNeeded += $SmallFace
+
+    }
+    $TotalNeeded
+
+########
+#Part 2#
+########
+
+$elfList = (gc  S:\Prod\AdventOfCode\RhysE\inputDay2.txt)
+$PerimOne = 0
+$PermiTwo = 0
+$PermiThree = 0
+$SmallestPerim = 0
+$Ribbon = 0 
+$ForBow = 0
+foreach ($present in $elfList) {
+    
+    $dimensions = [int[]]($present -split 'x')
+
+    $Width = $dimensions[0]
+    $Length = $dimensions[1]
+    $Height = $dimensions[2]
+
+    $SmallestPerim = (($Dimensions | sort)[0] *2) + (($Dimensions | sort)[1] *2)
+    $ForBow = ($Width * $Length * $Height)
+
+    $Ribbon += ($SmallestPerim + $ForBow)
+
+    }
+    $Ribbon
