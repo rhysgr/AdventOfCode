53313f04c250e4b7294e80dfffe021422f8a1938
rhysgr <31665049+rhysgr@users.noreply.github.com>
2018-02-13 17:10:00 +0000

    Updated

--- Day6.ps1
+++ Day6.ps1
@@ -6,7 +6,7 @@ $list = (gc  S:\Prod\AdventOfCode\RhysE\inputDay6.txt)
 $formattedList = @()
 foreach ($instruction in $list)
 {
-    $commands += ($instruction -replace '^((turn|toggle)( off| on)?) ([0-9,]+) through ([0-9,]+)$', '$2$3xx$4xx$5') -split 'xx'
+    $commands= ($instruction -replace '^((turn|toggle)( off| on)?) ([0-9,]+) through ([0-9,]+)$', '$2$3xx$4xx$5') -split 'xx'
     $formattedList += New-Object -TypeName PSObject -Property ([ordered]@{
         'Action' = $commands[0]
         'From' = $commands[1]
@@ -22,16 +22,19 @@ foreach ($instruction in $list)
     0..999 | % {$lights[$_] = @(0) *1000}
     foreach ($command in $formattedList)
     {
-        $xStart = (($command.from) -split ',')[4]
-        $yStart = (($command.from) -split ',')[5]
-        $xEnd = (($command.from) -split ',')[4]
-        $yEnd = (($command.from) -split ',')[5]
+        $xStart = (($command.from) -split ',')[0]
+        $yStart = (($command.from) -split ',')[1]
+        $xEnd = (($command.from) -split ',')[0]
+        $yEnd = (($command.from) -split ',')[1]
 
-        for ($x =0; $x -lt 1000;$x++)
+        for ($x =$xStart; $x -le $xEnd;$x++)
         {
-            for ($y = 0; $y -lt 1000;$y++)
+            for ($y = $yStart; $y -lt $yend;$y++)
             {
-                $lights[$x][$y] 
+                switch ($light[$x][$y]) {
+                    0 { $command.Action = 'turn off' }
+                    1 { $command.Action = 'turn on' }
+                }
             }
         }
     }
